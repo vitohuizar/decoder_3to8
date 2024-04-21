cabba58df719cd7d5452644122908621cd53c301
Vito Huizar <h.vito.v@gmail.com>
2024-04-21 14:53:19 -0700

    3to8 Decoder

---
+++ lab12.srcs/sim_1/new/decoder_3_to_8_testbench.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/17/2024 03:33:11 PM
+// Design Name: 
+// Module Name: decoder_3_to_8_testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder_3to8_testbench();
+  reg Enable, S1, S2, S0;
+  wire D7, D6, D5, D4, D3, D2, D1, D0;
+
+  decoder_3to8 dut(Enable, S2, S1, S0, D7, D6, D5, D4, D3, D2, D1, D0);
+
+  initial begin
+        Enable=0;    S2=0;  S1=0;   S0=0;
+    #1  Enable=1;    S2=0;  S1=0;   S0=0;
+    #1  Enable=1;    S2=0;  S1=0;   S0=1;
+    #1  Enable=1;    S2=0;  S1=1;   S0=0;
+    #1  Enable=1;    S2=0;  S1=1;   S0=1;
+    #1  Enable=1;    S2=1;  S1=0;   S0=0;
+    #1  Enable=1;    S2=1;  S1=0;   S0=1;
+    #1  Enable=1;    S2=1;  S1=1;   S0=0;
+    #1  Enable=1;    S2=1;  S1=1;   S0=1;
+  end
+endmodule
---
+++ lab12.srcs/sources_1/new/decoder_3_to_8.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/17/2024 03:14:08 PM
+// Design Name: 
+// Module Name: decoder_3_to_8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder_3to8(
+  input wire Enable,
+  input wire S2, S1, S0,
+  output wire D0, D1, D2, D3, D4, D5, D6, D7
+);
+
+  and(D0, Enable, ~S2, ~S1, ~S0);
+  and(D1, Enable, ~S2, ~S1, S0);
+  and(D2, Enable, ~S2, S1, ~S0);
+  and(D3, Enable, ~S2, S1, S0); 
+  and(D4, Enable, S2, ~S1, ~S0);
+  and(D5, Enable, S2, ~S1, S0);
+  and(D6, Enable, S2, S1, ~S0);
+  and(D7, Enable, S2, S1, S0);
+
+endmodule
